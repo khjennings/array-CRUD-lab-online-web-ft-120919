435cfa5316a351ecb8c03e05bb665b7724f64a0b
Khadijah Jennings <kjennings855@gmail.com>
2019-12-12 01:00:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,11 +11,11 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  create_an_array.unshift "wow"
+  create_an_array.unshift
 end
 
 def remove_element_from_end_of_array(array)
-  
+  create_an_array.pop
 end
 
 def remove_element_from_start_of_array(array)
